07e7fd7d11272a34ca4fc453269e25e87d90310b
Vailery <leraliabik8@gmail.com>
2022-03-04 17:50:37 +0300

    feat: add components and containers

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import styles from "./index.css";
+
+import { About, Footer, Header, Skills, Work } from "./container";
+import { Navbar } from "./components";
 
 const App = () => {
-  return <h1>App</h1>;
+  return (
+    <div className={styles.app}>
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
