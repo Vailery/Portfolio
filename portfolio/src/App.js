788fb52dc52472dd9412fd1f36eb687e3efe90b0
Vailery <leraliabik8@gmail.com>
2022-03-04 17:54:50 +0300

    feat: add some main styles

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./index.css";
+import styles from "./App.module.scss";
 
 import { About, Footer, Header, Skills, Work } from "./container";
 import { Navbar } from "./components";
