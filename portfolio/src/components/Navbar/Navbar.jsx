0f8d2397a59cb3f956ce1a9d5ca841291cc2249a
Vailery <leraliabik8@gmail.com>
2022-03-09 23:33:36 +0300

    refactor: add cv button and change styles

--- portfolio/src/components/Navbar/Navbar.jsx
+++ portfolio/src/components/Navbar/Navbar.jsx
@@ -13,6 +13,7 @@ const Navbar = () => {
       <div className="app__navbar-logo">
         <img src={images.logo} alt="logo" />
       </div>
+
       <ul className="app__navbar-links">
         {["home", "about", "work", "skills"].map((item) => (
           <li className="app__flex p-text" key={`link-${item}`}>
@@ -21,6 +22,10 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
+
+      <a href="#" className="p-text app__navbar-button" download>
+        Download CV
+      </a>
 
       <div className="app__navbar-menu">
         <HiMenuAlt4 onClick={() => setToggle(true)} />
@@ -39,6 +44,10 @@ const Navbar = () => {
                   </a>
                 </li>
               ))}
+
+              <a href="#" className="p-text menu-button" download>
+                Download CV
+              </a>
             </ul>
           </motion.div>
         )}
