868d328a9b79b31544ea384c0d5280dff75b2eec
Vailery <leraliabik8@gmail.com>
2022-03-11 00:26:21 +0300

    feat: add animation with lottie

--- portfolio/src/container/Footer/Footer.jsx
+++ portfolio/src/container/Footer/Footer.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import lottie from "lottie-web";
 import { HiOutlineMail, HiOutlinePhone } from "react-icons/hi";
 
 import { AppWrap, MotionWrap } from "../../wrapper";
 import "./Footer.scss";
 
 const Footer = () => {
+  const container = useRef(null);
+
+  useEffect(() => {
+    lottie.loadAnimation({
+      container: container.current,
+      renderer: "svg",
+      loop: true,
+      autoplay: true,
+      animationData: require("../../assets/computer.json"),
+    });
+  }, []);
+
   return (
     <>
       <h2 className="head-text">Take a coffee & chat with me</h2>
@@ -23,6 +36,8 @@ const Footer = () => {
           </a>
         </div>
       </div>
+
+      <div className="app__footer-animation" ref={container}></div>
     </>
   );
 };
